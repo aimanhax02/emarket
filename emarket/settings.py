9303c3d58c68cde030c4596b8e963ddcbb30a9e5
aiman <aimanshawal7@gmail.com>
2024-06-28 16:44:40 +0800

    more2

--- emarket/settings.py
+++ emarket/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'emarket.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'uXpZzrWhFOeDGaagqUpUKNtjPVNYcNeU',
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': '13602',
     }
 }
 
